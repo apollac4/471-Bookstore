d573d1d225565db182f088032a8a3c026612d98c
apollac4 <72466147+apollac4@users.noreply.github.com>
2021-12-01 13:22:44 -0500

    added most of admin functionality
    can't get purchases or last little bit of matching reviews to book titles to work

--- admin_tasks.php
+++ admin_tasks.php
@@ -4,8 +4,123 @@
 	<title>ADMIN TASKS</title>
 </head>
 <body>
+	<?php 
+
+		if(!array_key_exists("adminname",$_GET) and array_key_exists("adminname",$_REQUEST)){
+			$admin_name = $_REQUEST['adminname'];
+
+			$pin = $_REQUEST['pin'];
+			$user = 'root';
+			$pass = '';
+			$db = 'bookstore471';
+			$db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
+
+			if (mysqli_connect_errno()) {
+				echo "not connected! ".$db->error;
+				echo $db->error;
+			}
+
+			$query = "SELECT * FROM administrator WHERE Username='$admin_name' AND PIN = '$pin'";
+			$result = mysqli_query($db, $query);
+			$num_rows = mysqli_num_rows($result);
+			if (mysqli_num_rows($result)==0){
+				echo "No current user. Please sign up or enter correct sign-in information.";
+			}
+			else {
+				for ($i = 0; $i < $num_rows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					echo "Successfully logged in as " . $row["Username"];
+				}
+				$countOfUsers = 0;
+				$query = "SELECT * FROM customer";
+				$result = mysqli_query($db, $query);
+				$num_rows = mysqli_num_rows($result);
+				if (mysqli_num_rows($result)==0){
+					echo "No existing users.";
+				} else {
+					for ($i = 0; $i < $num_rows; $i++) {
+						$row = mysqli_fetch_assoc($result);
+						$countOfUsers+=1;
+					}
+					echo "<br><br><h1>There are currently " . $countOfUsers . " users registered.</h1>";
+				}
+				$query = "SELECT Genre, COUNT(*) FROM book GROUP BY Genre ORDER BY Genre";
+				$result = mysqli_query($db, $query);
+				$num_rows = mysqli_num_rows($result);
+				if (mysqli_num_rows($result)==0){
+					echo "No existing books in database.";
+				} else {
+					echo "<br><br>";
+					for ($i = 0; $i < $num_rows; $i++) {
+						$row = mysqli_fetch_assoc($result);
+						echo "<h1>There are currently " . $row['COUNT(*)'] . " books in " . $row['Genre'] . "</h1>";
+					}
+				}
+
+				// DATES
+
+				echo "<br><br>";
+				$query = "SELECT AVG(Total) AS 'average' FROM purchase WHERE 'Date' LIKE '01%'";
+				$result = mysqli_query($db, $query);
+				$num_rows = mysqli_num_rows($result);
+				for ($i = 0; $i < $num_rows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					echo "<h1>The average money spent this year in January on books is " . $row['average'] . "</h1>";
+				}
+				$query = "SELECT AVG(Total) AS 'average' FROM purchase WHERE 'Date' LIKE '02%'";
+				$result = mysqli_query($db, $query);
+				$num_rows = mysqli_num_rows($result);
+				for ($i = 0; $i < $num_rows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					echo "<h1>The average money spent this year in February on books is " . $row['average'] . "</h1>";
+				}
+				$query = "SELECT AVG(Total) AS 'average' FROM purchase WHERE 'Date' LIKE '11%'";
+				$result = mysqli_query($db, $query);
+				$num_rows = mysqli_num_rows($result);
+				for ($i = 0; $i < $num_rows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					echo "<h1>The average money spent this year in November on books is " . $row['average'] . "</h1>";
+				}
+				$query = "SELECT AVG(Total) AS 'average' FROM purchase WHERE 'Date' LIKE '12%'";
+				$result = mysqli_query($db, $query);
+				$num_rows = mysqli_num_rows($result);
+				for ($i = 0; $i < $num_rows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					echo "<h1>The average money spent this year in December on books is " . $row['average'] . "</h1>";
+				}
+
+				// WORKS WITHOUT LIKE
+
+				// $query = "SELECT AVG(Total) AS 'average' FROM purchase";
+				// $result = mysqli_query($db, $query);
+				// $num_rows = mysqli_num_rows($result);
+				// for ($i = 0; $i < $num_rows; $i++) {
+				// 	$row = mysqli_fetch_assoc($result);
+				// 	echo "<h1>The average money spent this year in December on books is " . $row['average'] . "</h1>";
+				// }
+
+				//BUG WHERE ONLY GRABS FIRST BOOK THEN COUNTS EVERY REVIEW FOR THAT FIRST BOOK INSTEAD OF MATCHING ISBN
+				$query = "SELECT book.Title, COUNT(review.ID) FROM book INNER JOIN review ON book.ISBN=review.ISBN;";
+				$result = mysqli_query($db, $query);
+				$num_rows = mysqli_num_rows($result);
+				for ($i = 0; $i < $num_rows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					echo "<br><br><h1>There are currently " . $row['COUNT(review.ID)'] . " reviews for " . $row['Title'] . "</h1>";
+				}
+				}
+				
+	 } else if(array_key_exists("adminname",$_GET)){
+			$admin_name = $_GET['adminname']; 
+		} else {
+			$admin_name = 'none';
+		}
+
+
+
+	?>
+
 	<table align="center" style="border:2px solid blue;">
-		<tr>
+		<!-- <tr>
 			<form action="manage_bookstorecatalog.php" method="post" id="catalog">
 			<td align="center">
 				<input type="submit" name="bookstore_catalog" id="bookstore_catalog" value="Manage Bookstore Catalog" style="width:200px;">
@@ -35,7 +150,7 @@
 		</tr>
 		<tr>
 		<td>&nbsp</td>
-		</tr>
+		</tr> -->
 		<tr>
 			<form action="index.php" method="post" id="exit">
 			<td align="center">
